33acb0461eddbf169a8d6064547882451e4422eb
Alberto Revuelta <alberto.re.arr@gmail.com>
2021-10-18 17:24:46 +0200

    Revert "Revert "Revert "App client token requests (#1)" (#2)" (#4)" (#5)
    This reverts commit 5d8c274df6d515a994e55ae012fc848aa025c2b0.

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :twitch_api,
   default_adapter: "https://dev.twitch.tv/docs/api/reference",
-  scopes: ""
+  authorization: System.get_env("authorization"),
+  client_id: System.get_env("client_id")
 
 import_config "#{Mix.env()}.exs"
